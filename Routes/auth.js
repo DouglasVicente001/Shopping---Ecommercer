166a82ef0be6fe32e82dfd728543c5084bc2b1c1
dsve <douglas.vicente@gft.com>
2022-09-14 15:43:17 -0300

    criandoJwtSenhaSeguraComScryptJs

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,12 +1,16 @@
 const router = require("express") . Router(); 
 const Usuario = require("../Models/Usuario")
+const CryptoJS = require("crypto-js")
 //Registro
 
 router.post("/registro", async (req, res) =>{
     const novoUsuario = new Usuario({
         nome: req.body.nome,
         email: req.body.email,
-        senha: req.body.email,
+        senha: CryptoJS.AES.encrypt(
+            req.body.senha, 
+            process.env.PASS_SEC
+            ).toString(),
     });
     try{
         const salvandoUsuario = await novoUsuario.save()
